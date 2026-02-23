12f8040f7755396fb6fd27a3ca6faf529dc772a6
Julian-Chris Reyes <julianchris.reyes@gmail.com>
2026-02-23 17:15:30 -0500

    initial-commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = req.url === '/' ? './index.html' : `.${req.url}`;
+    const extname = path.extname(filePath);
+    
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('File not found');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Game running at http://localhost:3000');
+});
